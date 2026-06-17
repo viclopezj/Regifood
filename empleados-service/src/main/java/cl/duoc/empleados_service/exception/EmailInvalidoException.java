77031fc1e7fb4cb41411cb2d0906992cb1cf9212
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:24:37 -0400

    Delete empleados-service directory

--- empleados-service/src/main/java/cl/duoc/empleados_service/exception/EmailInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.empleados_service.exception;
-
-public class EmailInvalidoException extends RuntimeException {
-    public EmailInvalidoException(String message) {
-        super(message);
-    }
-}
