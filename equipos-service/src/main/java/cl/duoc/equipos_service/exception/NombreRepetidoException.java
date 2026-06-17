6ba920e641da1471331538b14ff8e5fe1f249c77
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:39:51 -0400

    Delete equipos-service directory

--- equipos-service/src/main/java/cl/duoc/equipos_service/exception/NombreRepetidoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.equipos_service.exception;
-
-public class NombreRepetidoException extends RuntimeException {
-    public NombreRepetidoException(String message) {
-        super(message);
-    }
-}
--- equipos-service/src/main/resources/db/migration/V1__creacion_tabla_marcas.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE marcas
-(
-    codigo varchar(10) primary key,
-    nombre varchar(20) not null
-);
