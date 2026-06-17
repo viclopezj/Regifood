b62b7aa41377a9d52b424fc7acedce3ac918a4c7
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:24:46 -0400

    Delete equipos-service directory

--- equipos-service/src/main/java/cl/duoc/equipos_service/exception/CodigoMarcaExistenteException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.equipos_service.exception;
-
-public class CodigoMarcaExistenteException extends RuntimeException {
-    public CodigoMarcaExistenteException(String message) {
-        super(message);
-    }
-}
