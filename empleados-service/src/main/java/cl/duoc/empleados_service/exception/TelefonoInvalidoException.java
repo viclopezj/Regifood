a2dd93aea3953c0ac344a8a79f1440080d87cfa9
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:39:58 -0400

    Delete empleados-service directory

--- empleados-service/src/main/java/cl/duoc/empleados_service/exception/TelefonoInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.empleados_service.exception;
-
-public class TelefonoInvalidoException extends RuntimeException {
-    public TelefonoInvalidoException(String message) {
-        super(message);
-    }
-}
--- empleados-service/src/main/resources/db/migration/V1__creacion_tabla_empleados.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE empleados(
-    id bigint auto_increment primary key,
-    nombre varchar(50) not null,
-    apellido varchar(50) not null,
-    email varchar(100) not null,
-    fono varchar(15) not null,
-    salario int not null,
-    id_local bigint not null
-);
