1efac5fe3b7e7a2e6271c77dbe2f46f67daf4efa
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:40:45 -0400

    Delete inventarios-service directory

--- inventarios-service/src/main/resources/db/migration/V1__creacion_tabla_inventarios.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE inventarios(
-    id bigint auto_increment primary key,
-    nombre_insumo varchar(100) not null,
-    cantidad_actual int not null,
-    id_local bigint not null,
-    id_proveedor bigint not null
-);
