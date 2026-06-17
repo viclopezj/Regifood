99a1745e9d08124c997bcf7b83e6e6bd4e602db7
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:41:18 -0400

    Delete menus-service directory

--- menus-service/src/main/java/cl/duoc/menus_service/exception/NombreCategoriaRepetidoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.menus_service.exception;
-
-public class NombreCategoriaRepetidoException extends RuntimeException {
-    public NombreCategoriaRepetidoException(String message) {
-        super(message);
-    }
-}
--- menus-service/src/main/resources/db/migration/V1__creacion_tabla_categorias.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE categorias
-(
-    codigo varchar(10) primary key,
-    nombre varchar(20) not null
-);
