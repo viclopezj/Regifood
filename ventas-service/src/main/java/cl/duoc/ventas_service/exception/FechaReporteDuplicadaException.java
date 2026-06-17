31ee7170bcb9e96815595878bd47691f35746d56
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:41:33 -0400

    Delete ventas-service directory

--- ventas-service/src/main/java/cl/duoc/ventas_service/exception/FechaReporteDuplicadaException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.ventas_service.exception;
-
-public class FechaReporteDuplicadaException extends RuntimeException {
-    public FechaReporteDuplicadaException(String message) {
-        super(message);
-    }
-}
