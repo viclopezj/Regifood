f741e634b493831edcf5c346697047172621b4d1
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:40:52 -0400

    Delete locales-service directory

--- locales-service/src/main/resources/db/migration/V1__creacion_tabla_locales.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE locales(
-    id bigint auto_increment primary key,
-    nombre varchar(50) not null,
-    comuna varchar(50) not null,
-    direccion varchar(100) not null,
-    hora_apertura time not null,
-    hora_cierre time not null,
-    id_gerente bigint not null
-);
