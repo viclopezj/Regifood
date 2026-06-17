991138e903375365802660f64008876b5fa0e2f2
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:25:57 -0400

    Delete proveedores-service directory

--- proveedores-service/src/main/java/cl/duoc/proveedores_service/exception/EmailInvalidoException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.proveedores_service.exception;
-
-public class EmailInvalidoException extends RuntimeException {
-    public EmailInvalidoException(String message) {
-        super(message);
-    }
-}
