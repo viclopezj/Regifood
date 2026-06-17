27b6d076410f8a5118cd3a6bd50326c804966319
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:25:50 -0400

    Delete menus-service directory

--- menus-service/src/main/java/cl/duoc/menus_service/exception/CodigoCategoriaExistenteException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.menus_service.exception;
-
-public class CodigoCategoriaExistenteException extends RuntimeException {
-    public CodigoCategoriaExistenteException(String message) {
-        super(message);
-    }
-}
--- menus-service/src/main/java/cl/duoc/menus_service/exception/MenuDuplicadoEnLocalException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.menus_service.exception;
-
-public class MenuDuplicadoEnLocalException extends RuntimeException {
-    public MenuDuplicadoEnLocalException(String message) {
-        super(message);
-    }
-}
