2bf664a81050979e9c4e7921a2bcdab941cc3306
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:25:23 -0400

    Delete inventarios-service directory

--- inventarios-service/src/main/java/cl/duoc/inventarios_service/exception/InsumoDuplicadoEnLocalException.java
+++
@@ -1,7 +0,0 @@
-package cl.duoc.inventarios_service.exception;
-
-public class InsumoDuplicadoEnLocalException extends RuntimeException {
-    public InsumoDuplicadoEnLocalException(String message) {
-        super(message);
-    }
-}
