951710c891c831a31206c503138993aac9914a84
viclopezj <vic.lopezj@duocuc.cl>
2026-06-17 13:26:05 -0400

    Delete ventas-service directory

--- ventas-service/src/main/java/cl/duoc/ventas_service/dto/LocalDTO.java
+++
@@ -1,9 +0,0 @@
-package cl.duoc.ventas_service.dto;
-
-import lombok.Data;
-
-@Data
-public class LocalDTO {
-    private Long id;
-    private String nombre;
-}
--- ventas-service/src/main/resources/db/migration/V1__creacion_tabla_ventas.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE ventas(
-    id bigint auto_increment primary key,
-    id_local bigint not null,
-    fecha_reporte date not null,
-    venta_minimas int not null,
-    venta_maximas int not null,
-    venta_promedio int not null
-);
