750289d76b33df7211dfc39d98d0e8a4e1846b70
Vicente <vic.lopezj@duocuc.cl>
2026-06-17 20:16:44 -0400

    Actualizacion Swagger por Api-Gateway

--- locales-service/src/main/java/cl/duoc/locales_service/LocalesServiceApplication.java
+++ locales-service/src/main/java/cl/duoc/locales_service/LocalesServiceApplication.java
@@ -1,5 +1,8 @@
 package cl.duoc.locales_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
